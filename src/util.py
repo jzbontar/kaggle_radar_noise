b492267deec99c4c976e1d5ed4fc1ae9529e5580
Jure Zbontar <jure.zbontar@gmail.com>
2016-07-05 18:43:08 -0400

    Add Steiner Smith

--- src/util.py
+++ src/util.py
@@ -1,14 +1,16 @@
 import numpy as np
 import scipy.spatial
 
+# Inspired by wradlib library's ipol and togrid functions.
+def ipol_nearest(src, trg, data):
+    tree = scipy.spatial.cKDTree(src)
+    dists, ix = tree.query(trg, k=1)
+    return data[ix]
 
-# Inspired by wradlib library's togrid function
 def togrid(polar, x, y, gridsize=1024, lim=460):
     src = np.column_stack((x.ravel(), y.ravel()))
     grid = np.linspace(-lim * 1000, lim * 1000, gridsize)
     mgrid = np.meshgrid(grid, grid[::-1])
     trg = np.column_stack((mgrid[0].ravel(), mgrid[1].ravel()))
-    tree = scipy.spatial.cKDTree(src)
-    dists, ix = tree.query(trg, k=1)
-    grid = polar.ravel()[ix]
+    grid = ipol_nearest(src, trg, polar.ravel())
     return grid.reshape(gridsize, gridsize)
